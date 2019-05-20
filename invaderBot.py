6538dd0ffe7b50ec742e3c368f4c36fc727bfe1d
semih <semih.iseri@gmail.com>
2019-05-20 19:11:12 +0300

    python API MVP

---
+++ invaderBot.py
@@ -0,0 +1,43 @@
+
+from controller import Robot
+
+
+class invaderBot():
+    def __init__(self): # this might be overly complicated :/
+        self.robot = Robot()
+        
+        def setup_motors(robot, motor_names):
+            return [robot.getMotor(motor_name) for motor_name in motor_names]
+        
+        (self.m_fl, self.m_fr, self.m_rl, self.m_rr) = setup_motors(self.robot, ["front_left", "front_right", "rear_left", "rear_right"])
+        
+        self.leftMotor = 0
+        self.rightMotor = 0
+        
+        self.timestep = int(self.robot.getBasicTimeStep())
+        
+    def setup(self): # let's have this here for cross-language compatibility -it will be needed for C
+        pass
+    
+    def doTimeStep(self):
+        return (self.robot.step(self.timestep) != -1)
+    
+    def setMotors(self, left, right):
+        def _setMotors(inp, motors):
+            mult = 1 if inp > 0 else -1
+            for motor in motors:
+                motor.setPosition(mult*float('+inf'))
+                motor.setVelocity(inp)
+        
+        def setAllMotors(speeds):
+            motors = (self.m_fl, self.m_rl, self.m_fr, self.m_rr)
+            for i, speed in enumerate(speeds):
+                _setMotors(speed, [motors[i]])
+            
+        setAllMotors((left, left, right, right))
+    
+    def setLeftMotor(self, value):
+        pass
+    
+    def setRightMotor(self, value):
+        pass
